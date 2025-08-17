49b1724577b74161a45a6d58c4642dce60739386
lucotuslim <limcheeyong1978@gmail.com>
2025-08-17 22:00:47 +0800

    almost

--- app/appconfiguration/controldb/page.tsx
+++ app/appconfiguration/controldb/page.tsx
@@ -1,5 +1,6 @@
 "use client"
-import { columns } from "./columns";
+import React from "react";
+import { columns as baseColumns } from "./columns";
 import { DataTable } from "./data-table";
 import {IapiControlDbUrl} from "./interfaces"
 
@@ -8,17 +9,27 @@ const apiControlDbUrl: IapiControlDbUrl[] = process.env.NEXT_PUBLIC_CONTROLSERVE
   : [];
 
 export default function ApiDiv() {
-  // return (
-  //   <div>
-  //     {apiControlDbUrl.map(
-  //       (url, index) => <pre key={index}>{url}</pre>
-  //     )}
-  //   </div>
-  // );
+  const dynamicColumns = [
+    ...baseColumns,
+    {
+      accessorKey: "extra",
+      header: "Extra Info",
+      cell: ({ row }: { row: { original: IapiControlDbUrl } }) => {
+        const [result, setResult] = React.useState<string>("Loading...");
+        React.useEffect(() => {
+          fetch(row.original.Controldburl)
+            .then(res => res.ok ? res.text() : Promise.reject("Error"))
+            .then(data => setResult(data))
+            .catch(() => setResult("Error"));
+        }, [row.original.Controldburl]);
+        return <span>{result}</span>;
+      },
+    },
+  ];
 
     return (
     <div className="container mx-auto py-10">
-      <DataTable columns={columns} data={apiControlDbUrl} />
+      <DataTable columns={dynamicColumns} data={apiControlDbUrl} />
     </div>
   )
 }
