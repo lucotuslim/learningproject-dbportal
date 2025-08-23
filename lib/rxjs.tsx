2ffcea86e0e44b0ea92843c1d2e7454ba9260fbe
lucotuslim <limcheeyong1978@gmail.com>
2025-08-23 11:37:09 +0800

    moved to rxjs

--- lib/rxjs.tsx
+++ lib/rxjs.tsx
@@ -1,9 +1,10 @@
-import { of, map } from 'rxjs';
-import { ApiInterface } from '@/interfaces/generic';
+import { catchError, forkJoin, map, of } from "rxjs";
+import { ApiInterface, ISqlServerInstance } from '@/interfaces/generic';
 import { fromFetch } from 'rxjs/fetch';
 import { switchMap, throwError } from 'rxjs';
+import { IapiControlDbUrlWithSqlServer } from "@/interfaces/controldb";
 
-export function serverwithapi(servername: string) { 
+export function serverwithapi(servername: string) {
   return of(servername).pipe(
     map(name => ({
       Servername: name,
@@ -13,7 +14,7 @@ export function serverwithapi(servername: string) {
 }
 
 export function ApiRequestRxjs<T>(fetchUrl: string,
-  options?: RequestInit) { 
+  options?: RequestInit) {
   return fromFetch(fetchUrl, options).pipe(
     switchMap(response => {
       if (!response.ok) {
@@ -23,3 +24,34 @@ export function ApiRequestRxjs<T>(fetchUrl: string,
     })
   );
 }
+
+export function ApiGetControlDbRxjs<T>(
+  apiControlDbUrl: { Controldburl: string }[]
+) {
+  return forkJoin(
+    apiControlDbUrl.map((entry) =>
+      ApiRequestRxjs<ISqlServerInstance>(entry.Controldburl).pipe(
+        map((result) =>
+          result.items.map((item) => ({
+            ...item,
+            message: result.message || "",
+            error: result.error || false,
+            Controldburl: entry.Controldburl,
+          }))
+        ),
+        catchError((err) =>
+          of([
+            {
+              Controldburl: entry.Controldburl,
+              message: err.message || "Unknown error",
+              error: true,
+            } as IapiControlDbUrlWithSqlServer,
+          ])
+        )
+      )
+    )
+  ).pipe(
+    map((results) => results.flat())
+  );
+}
+
