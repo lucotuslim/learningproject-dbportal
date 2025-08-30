73785b13d41ff576a877955e7862c1a68a00630d
lucotuslim <limcheeyong1978@gmail.com>
2025-08-30 12:18:53 +0800

    Update generic.tsx

--- lib/rxjs/generic.tsx
+++ lib/rxjs/generic.tsx
@@ -3,14 +3,16 @@ import { ApiInterface } from '@/interfaces/generic';
 import { switchMap, throwError, map, of } from 'rxjs';
 import { fromFetch } from 'rxjs/fetch';
 
-export function ApiRequestRxjs<T>(fetchUrl: string,
-  options?: RequestInit) {
+export function ApiRequestRxjs<T>(fetchUrl: string, options?: RequestInit, UrlType?: string) {
   return fromFetch(fetchUrl, options).pipe(
     switchMap(response => {
       if (!response.ok) {
         return throwError(() => new Error(`API error: ${response.status}`));
       }
-      return response.json() as Promise<ApiInterface<T>>;
+      return response.json().then((json: ApiInterface<T>) => ({
+        ...json,
+        UrlType: UrlType ?? null
+      })) as Promise<ApiInterface<T> & { UrlType?: string }>;
     })
   );
 }
