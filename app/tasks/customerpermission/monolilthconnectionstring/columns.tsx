40a2ad864bc6fd896aeccd8de8997ed6393bae02
lucotuslim <limcheeyong1978@gmail.com>
2026-01-11 17:51:05 +0800

    completed

---
+++ app/tasks/customerpermission/monolilthconnectionstring/columns.tsx
@@ -0,0 +1,105 @@
+"use client"
+import { ColumnDef } from "@tanstack/react-table"
+// import { IDatabaseInfo } from "@/interfaces/databases"
+// import { IapiInfo } from "@/interfaces/generic"
+// import { ApiRequestRxjs } from '@/lib/rxjs/generic'
+// import Link from "next/link"
+// import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+// import { Button } from "@/components/ui/button"
+// import { MoreHorizontal } from "lucide-react"
+// import { ConfirmationDialog, useConfirmationDialog } from "@/components/confirmation-dialog"
+// import { useState } from "react"
+// import { toast } from "sonner"
+// import { Row } from "@tanstack/react-table"
+import { ICustomerSecurityGroup } from "../interfaces"
+// Recovery Model Cell Component
+
+// // Actions Cell Component
+// function ActionsCell({ row, refreshData }: { 
+//   row: Row<IapiInfo<IDatabaseInfo & { MachineName: string }>>
+//   refreshData: () => void 
+// }) {
+//   const { open, openDialog, setOpen } = useConfirmationDialog()
+
+//   const database = row.original
+//   const machineName = row.original.MachineName
+
+//   const handleDelete = () => {
+//     console.log('deleting database', database.name, 'on server', machineName)
+//     const url = `http://${machineName}api:3000/api/databases/${database.database_id}`
+//     ApiRequestRxjs(url, "Database", {
+//       method: "DELETE", 
+//       headers: {
+//         "Content-Type": "application/json",
+//       }
+//     }).subscribe({
+//       next: () => {
+//         toast("Drop Database", {
+//           description: `Dropped database ${database.name} on server ${machineName}`,
+//           action: {
+//             label: "Close",
+//             onClick: () => console.log("Undo"),
+//           }
+//         })
+//         refreshData()
+//       },
+//       error: (err) => {
+//         console.error("Delete error:", err)
+//       }
+//     })
+//   }
+
+//   return (
+//     <>
+//       <DropdownMenu>
+//         <DropdownMenuTrigger asChild>
+//           <Button variant="ghost" className="h-8 w-8 p-0">
+//             <span className="sr-only">Open menu</span>
+//             <MoreHorizontal />
+//           </Button>
+//         </DropdownMenuTrigger>
+//         <DropdownMenuContent align="end">
+//           <DropdownMenuLabel>Actions</DropdownMenuLabel>
+//           <DropdownMenuItem onClick={openDialog}>
+//             Drop Database
+//           </DropdownMenuItem>
+//           <DropdownMenuSeparator />
+//           <DropdownMenuItem>Migrate Database</DropdownMenuItem>
+//         </DropdownMenuContent>
+//       </DropdownMenu>
+
+//       <ConfirmationDialog
+//         open={open}
+//         onOpenChange={setOpen}
+//         description={`This action cannot be undone. This will permanently delete your database <b>${database.name}</b>.`}
+//         onConfirm={handleDelete}
+//       />
+//     </>
+//   )
+// }
+
+export const columns = (): ColumnDef<ICustomerSecurityGroup>[] => [
+  //  [CustomerSecurityGroupsId]
+  //       ,[GroupSID]
+  //       ,[Environment]
+  //       ,[Namespace]
+  //       ,[ClientId]
+  //       ,[GroupName]
+  //       ,[MetaData]
+  //       ,[CollectedTimestamp]
+  //       ,[Permission]
+  //       ,[IsDeleted]
+  { accessorKey: "GroupSID", header: "GroupSID" },
+  { accessorKey: "Environment", header: "Environment" },
+
+  { accessorKey: "Namespace", header: "Namespace" },
+  { accessorKey: "ClientId", header: "ClientId" },
+
+  { accessorKey: "GroupName", header: "GroupName" },
+  { accessorKey: "MetaData", header: "MetaData" },
+  { accessorKey: "CollectedTimestamp", header: "CollectedTimestamp" },
+  { accessorKey: "Permission", header: "Permission" },
+  { accessorKey: "IsDeleted", header: "IsDeleted" },
+]
+
+
