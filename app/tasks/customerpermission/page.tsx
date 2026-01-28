697a842f1784fe955e7781be20b831f283133a2d
lucotuslim <limcheeyong1978@gmail.com>
2026-01-28 14:24:55 +0800

    commit

--- app/tasks/customerpermission/page.tsx
+++ app/tasks/customerpermission/page.tsx
@@ -16,38 +16,26 @@ export default function Page() {
   return (
     <div className="w-full gap-6 m-6">
       <Tabs defaultValue="customersecuritygroup" className="w-full">
-
         <TabsList className="w-full">
-
           <TabsTrigger value="customersecuritygroup" className="w-full">
             Customer Security Group
           </TabsTrigger>
-
           <TabsTrigger value="connectionstring" className="w-full">
             Connection String
           </TabsTrigger>
           <TabsTrigger value="settings" className="w-full">
             Settings
           </TabsTrigger>
-
-
         </TabsList>
-
         <TabsContent value="customersecuritygroup" className="w-full" >
           <CustomerSecurityGroup />
         </TabsContent>
-
-
         <TabsContent value="connectionstring" className="w-full" >
           <ConnectionString />
         </TabsContent>
-
-
         <TabsContent value="settings" className="w-full" >
           <Settings />
         </TabsContent>
-
-
       </Tabs>
     </div>
   );
